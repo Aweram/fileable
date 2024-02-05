6dd0f81c4c91bece7289d2455480166ddcc89e51
Labrock <vladimirpeshkov5@yandex.ru>
2024-02-05 19:08:48 +0300

    add should image trait

--- src/Livewire/ImageIndexWire.php
+++ src/Livewire/ImageIndexWire.php
@@ -3,12 +3,15 @@
 namespace Aweram\Fileable\Livewire;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Model;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class ImageIndexWire extends Component
 {
     use WithPagination;
+
+    public Model $model;
 
     public function render(): View
     {
---
+++ src/Traits/ShouldImage.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Aweram\Fileable\Traits;
+
+use Aweram\Fileable\Models\FileModel;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
+
+trait ShouldImage
+{
+    protected static function bootShouldImage()
+    {
+        static::deleted(function (Model $model) {
+            $model->clearImage(true);
+        });
+    }
+
+    protected function getImageKey()
+    {
+        return ! empty($this->imageKey) ? $this->imageKey : "image_id";
+    }
+
+    /**
+     * @return string
+     */
+    public function getImageFileClassAttribute(): string
+    {
+        return config("fileable.customFileModel") ?? FileModel::class;
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function image(): BelongsTo
+    {
+        return $this->belongsTo($this->image_file_class, $this->getImageKey());
+    }
+
+    /**
+     * @param string|null $path
+     * @param string $inputName
+     * @param string $field
+     * @return void
+     */
+    public function uploadImage(string $path = null, string $inputName = "image", string $field = "title"): void
+    {
+        if (! request()->hasFile($inputName)) return;
+        $file = request()->file($inputName);
+        $this->storeImage($file, $path, $field);
+    }
+
+    /**
+     * @param TemporaryUploadedFile|null $file
+     * @param string|null $path
+     * @param string $field
+     * @return void
+     */
+    public function livewireImage(TemporaryUploadedFile $file = null, string $path = null, string $field = "title"): void
+    {
+        if (! $file) return;
+        $this->storeImage($file, $path, $field);
+    }
+
+    /**
+     * Удалить изображение.
+     *
+     * @param bool $deleted
+     * @return void
+     */
+    public function clearImage(bool $deleted = false): void
+    {
+        $image = $this->image;
+        if (! empty($image)) $image->delete();
+        if (! $deleted) {
+            $this->image()->disassociate();
+            $this->save();
+        }
+    }
+
+    /**
+     * @param UploadedFile $file
+     * @param string|null $path
+     * @param string $field
+     * @return void
+     */
+    private function storeImage(UploadedFile $file, string $path = null, string $field = "title"): void
+    {
+        if (! $path) $path = $this->getTable();
+        // Удалить старое изображение.
+        $this->clearImage();
+        // Получить расширение файла.
+        $mime = $file->getClientOriginalExtension();
+        // Сохранить файл.
+        $fileName = Str::random(40) . "." . $mime;
+        $path = $file->storeAs($path, $fileName);
+        // Получить имя файла.
+        if (! empty($this->{$field})) $name = $this->{$field};
+        else {
+            $name = $file->getClientOriginalName();
+            $name = str_replace(".{$mime}", "", $name);
+        }
+        // Тип файла изображение.
+        $type = "image";
+        // Создание файла.
+        $image = $this->image_file_class::create(compact("path", "name", "mime", "type"));
+        $this->image()->associate($image);
+        $this->save();
+    }
+}

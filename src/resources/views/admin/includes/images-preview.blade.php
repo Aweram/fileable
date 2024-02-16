5dbb9a97f5f5b6578cba7cc034bc153f997330f8
Labrock <vladimirpeshkov5@yandex.ru>
2024-02-16 17:52:05 +0300

    make preview styles

--- src/resources/views/admin/includes/images-preview.blade.php
+++ src/resources/views/admin/includes/images-preview.blade.php
@@ -4,31 +4,35 @@
             $imgError = $errors->has("image") && $key === 0;
             $nameError = $errors->has("name") && $key === 0;
         @endphp
-        <div class="col w-1/3">
+        <div class="col w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/5 mb-indent">
             @if ($item["preview"])
                 <img src="{{ $item['preview'] }}" alt="preview"
-                     class="w-28 h-28 object-cover rounded border border-transparent {{ $imgError ? "border-danger" : "" }}">
+                     class="w-full h-60 object-cover rounded-lg border border-transparent mb-indent-half {{ $imgError ? "border-danger" : "" }}">
             @else
-                <div>No preview</div>
+                <div class="w-full h-60 mb-indent-half rounded-lg bg-info-lighten flex justify-center items-center">
+                    <x-tt::ico.img class="text-white" width="36" height="48" />
+                </div>
             @endif
-            @if ($imgError) <x-tt::form.error name="image" /> @endif
+            @if ($imgError) <x-tt::form.error name="image" class="mb-indent-half" /> @endif
 
-            <input class="form-control {{ $nameError ? "border-danger" : "" }}"
-                   type="text" aria-label="Name"
-                   wire:model="forUpload.{{ $key }}.name"
-                   @if ($uploadProcess) disabled @endif>
-            @if ($nameError) <x-tt::form.error name="name" />@endif
+            <div class="flex justify-between">
+                <input class="form-control {{ $nameError ? "border-danger" : "" }} mr-indent-half"
+                       type="text" aria-label="Name"
+                       wire:model="forUpload.{{ $key }}.name"
+                       @if ($uploadProcess) disabled @endif>
 
-            <button type="button" class="btn btn-outline-danger"
-                    wire:click.prevent="deleteImageItem({{ $key }})"
-                    @if ($uploadProcess) disabled @endif>
-                Delete
-            </button>
+                <button type="button" class="btn btn-danger px-btn-x-ico"
+                        wire:click.prevent="deleteImageItem({{ $key }})"
+                        @if ($uploadProcess) disabled @endif>
+                    <x-tt::ico.trash />
+                </button>
+            </div>
+            @if ($nameError) <x-tt::form.error class="mt-indent-half" name="name" />@endif
         </div>
     @endforeach
     @if (count($forUpload))
         <div class="col w-full">
-            <button type="button" class="btn"
+            <button type="button" class="btn btn-primary btn-lg w-full"
                     wire:click="startUploadImages" wire:loading.attr="disabled"
                     @if ($uploadProcess) disabled @endif>
                 Load

85d97ba2d642f6cdb158cd3193b270b984762c01
Labrock <vladimirpeshkov5@yandex.ru>
2024-02-16 16:17:44 +0300

    change gallery table view

--- src/resources/views/admin/includes/image-table.blade.php
+++ src/resources/views/admin/includes/image-table.blade.php
@@ -2,7 +2,6 @@
     <x-slot name="head">
         <tr>
             <x-tt::table.heading class="text-left">{{ __("File") }}</x-tt::table.heading>
-            <x-tt::table.heading>{{ __("Actions") }}</x-tt::table.heading>
         </tr>
     </x-slot>
     <x-slot name="body">
@@ -12,51 +11,54 @@
                 wire:key="{{ $item->id }}">
                 <td>
                     <div class="flex">
+                        @if (!$hasSearch)
+                            <div class="flex items-center">
+                                <x-tt::ico.bars drag-gallery-grab class="text-secondary mr-indent cursor-grab" />
+                            </div>
+                        @endif
+
                         <a href="{{ route("thumb-img", ["template" => "original", "filename" => $item->file_name]) }}"
                            target="_blank" class="block mr-indent basis-auto">
                             <img src="{{ route("thumb-img", ["template" => "gallery-preview", "filename" => $item->file_name]) }}"
                                  alt="{{ $item->name }}" class="rounded-lg">
                         </a>
-                        <div class="flex flex-col justify-between items-start">
-                            @if ($displayName && $imageId == $item->id)
-                                <div class="flex justify-start">
-                                    <input type="text" class="form-control form-control-sm rounded-r-none border-r-0 @if ($errors->has('name')) border-danger @endif"
-                                           aria-label="File name"
-                                           wire:model="name">
-                                    <button type="button" class="btn btn-sm btn-outline-dark rounded-r-none rounded-l-none border-r-0"
-                                            wire:click="closeEditName">
-                                        {{ __("Cancel") }}
+
+                        <div class="flex flex-col justify-between">
+                            <div>
+                                @if ($displayName && $imageId == $item->id)
+                                    <div class="flex justify-start">
+                                        <input type="text" class="form-control form-control-sm rounded-r-none border-r-0 @if ($errors->has('name')) border-danger @endif"
+                                               aria-label="File name"
+                                               wire:model="name">
+                                        <button type="button" class="btn btn-sm btn-outline-dark rounded-r-none rounded-l-none border-r-0"
+                                                wire:click="closeEditName">
+                                            {{ __("Cancel") }}
+                                        </button>
+                                        <button type="button" class="btn btn-sm btn-outline-primary rounded-l-none"
+                                                wire:click="updateName">
+                                            {{ __("Update") }}
+                                        </button>
+                                    </div>
+                                    <x-tt::form.error name="name" />
+                                @else
+                                    <button type="button" class="underline" wire:click="showEditName({{$item->id}}, '{{$item->name}}')">
+                                        {{ $item->name }}
                                     </button>
-                                    <button type="button" class="btn btn-sm btn-outline-primary rounded-l-none"
-                                            wire:click="updateName">
-                                        {{ __("Update") }}
-                                    </button>
-                                </div>
-                                <x-tt::form.error name="name" />
-                            @else
-                                <button type="button" class="underline" wire:click="showEditName({{$item->id}}, '{{$item->name}}')">
-                                    {{ $item->name }}
-                                </button>
-                            @endif
-                            <ul class="text-secondary text-sm">
-                                <li>{{ $item->human_size }}</li>
-                                <li>{{ __("Loaded") }} {{ $item->created_human }}</li>
-                            </ul>
-                        </div>
-                    </div>
-                </td>
-                <td>
-                    <div class="flex justify-center">
-                        <button type="button" class="btn btn-danger px-btn-x-ico"
-                                wire:loading.attr="disabled"
-                                wire:click="showDelete({{ $item->id }})">
-                            <x-tt::ico.trash />
-                        </button>
-                        @if (!$hasSearch)
-                            <div drag-gallery-grab class="btn btn-outline-dark cursor-move ml-indent-half">
-                                Drag
+                                @endif
+
+                                <ul class="text-secondary text-sm flex space-x-indent-half">
+                                    <li>{{ $item->human_size }}</li>
+                                    <li>{{ __("Loaded") }} {{ $item->created_human }}</li>
+                                </ul>
                             </div>
-                        @endif
+                            <div>
+                                <button type="button" class="underline text-sm"
+                                        wire:loading.attr="disabled"
+                                        wire:click="showDelete({{ $item->id }})">
+                                    {{ __("Delete") }}
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </td>
             </tr>
